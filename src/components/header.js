8703b673b3eac5fc510393532d715aa8a6102eaf
pillieshwar <pillieshwar@gmail.com>
2021-02-22 20:51:23 -0800

    Added Material-ui and updated header

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,70 @@
 import React from "react";
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import SettingsIcon from '@material-ui/icons/Settings';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 // import Logo from "../static/logonew.png";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    textAlign: 'center',
+  }
+}));
+
 export default function Header() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
   return (
-    <div className="myHeader">
-     <p style={{marginLeft:"0.5rem",marginTop:"1rem",fontWeight:"600", fontSize:"1.3rem"}}>Synchrophasor Anomaly/Event Detection and Classification </p>
-      <button className="userSettings"
-        onClick={() => alert("Settings")}
+    <div className={classes.root}>
+      <Grid container className="highlight-color">
+        <Grid item xs={11} >
+          <Paper elevation={0} className={classes.paper}>
+            {/* <Header /> */}
+            <h2 className="highlight-color">
+            Synchrophasor Anomaly/Event Detection and Classification</h2>
+          </Paper>
+        </Grid>
+        <Grid item xs={1} >
+        <Fab aria-controls="header" color="secondary" style={{marginTop:"0.4rem"}} onClick={handleClick} aria-label="add" aria-haspopup="true">
+          <SettingsIcon fontSize="medium" />
+        </Fab>
+        <Menu
+        id="header"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
       >
-        User Settings
-      </button>
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+        </Grid>
+      </Grid>
     </div>
+    // <div className="myHeader">
+    //  <p style={{marginLeft:"0.5rem",marginTop:"1rem",fontWeight:"600", fontSize:"1.3rem"}}>Synchrophasor Anomaly/Event Detection and Classification </p>
+    //   <button className="userSettings"
+    //     onClick={() => alert("Settings")}
+    //   >
+    //     User Settings
+    //   </button>
+    // </div>
   );
 }
